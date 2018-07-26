6b01a472f16aacad4d36ef7ef889ec4fdccaaa4f
Icaro Cristofolini <icaroaj@hotmail.com>
2018-07-26 10:48:55 -0300

    Versao funcional 1

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,16 +1,3 @@
-const servicos = [
-    {servico: "Limpeza interna", preco: 45},
-    {servico: "Formatação", preco: 40},
-    {servico: "Formatação + Limpeza interna", preco: 65},
-    {servico: "Troca de Tela", preco: 85},
-    {servico: "Troca de Placa Mãe", preco: 100},
-    {servico: "Troca de HD/Memória", preco: 50},        
-  ];
-  let total = 0;
-  /*let servicos[
-
-  ];*/
-
   function buildHeader(servicos){
     let tHead = document.createElement('thead')
     let anyServico = servicos [0]
@@ -25,41 +12,12 @@ const servicos = [
     return tHead;
   }
 
-  /*function buildBody(servicos){
-    let tBody = document.createElement('tbody')
-    let total = 0;
-    
-    for (servico of servicos){
-      let tr = document.createElement('tr')
-      let td1 = document.createElement('td')
-      td1.innerText = servico.servico
-      let td2 = document.createElement('td')
-      td2.innerText = ("R$ ") + servico.preco
-
-      total += servico.preco;
-
-      tr.appendChild(td1);
-      tr.appendChild(td2);
-      tBody.appendChild(tr);
-    }
-
-    return tBody;
-  }*/
+  let total = 0;
 
   function updateTotal(value){
-    total += value;
-
-
+    let int = parseInt(value, 10)
+    total += int;
   }
-
-  function valorTotal(servicos){
-    let vTotal = document.createElement('vTotal')
-    let tr = document.createElement('tr')
-
-
-    return tValue;
-  }
-
 
 document.addEventListener('DOMContentLoaded',function() {
     document.querySelector('#lista').onchange=changeEventHandler;
@@ -76,9 +34,12 @@ function changeEventHandler(event) {
     td2.innerText = "R$ " + this.value
 
     updateTotal(this.value);
+    let tv = document.getElementById('TotalValue')
+    tv.innerText = "R$ " + total;
 
     tr.appendChild(td1);
     tr.appendChild(td2);
     let list = document.getElementById('TableBody');
     list.insertBefore(tr, list.childNodes[0]);
+    
 }
